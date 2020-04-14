a8a1dc0d46f7c44d261cfcc6caf32eeb76d5778a
bhassi01 <suyal45@gmail.com>
2020-04-14 23:30:07 +0530

    First proper commit

---
+++ GitPractice/src/First_Programmes/Hello_World.java
@@ -0,0 +1,11 @@
+package First_Programmes;
+
+public class Hello_World {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Helloworld");
+
+	}
+
+}
